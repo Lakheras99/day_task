bd08af23bd0706419ffda911318da977342b7237
Lakheras99 <49496887+Lakheras99@users.noreply.github.com>
2024-04-16 22:26:46 +0530

    2nd commit

---
+++ lib/Screens/splash_screen.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      backgroundColor: Color(0xFF212832),
+      body: SafeArea(
+        child: Text("Hello"),
+      ),
+    );
+  }
+}
